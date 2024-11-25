817545e53509d268c67371ffd3e30e4690f86461
Dak Lod <trandacloc123@gmail.com>
2024-11-25 09:43:24 +0700

    add task_history_type

--- server/Context/SupabaseContext.cs
+++ server/Context/SupabaseContext.cs
@@ -306,6 +306,7 @@ public partial class SupabaseContext : DbContext
             entity.Property(e => e.CreatedBy).HasColumnName("created_by");
             entity.Property(e => e.Description).HasColumnName("description");
             entity.Property(e => e.TaskId).HasColumnName("task_id");
+            entity.Property(e => e.Type).HasColumnName("type");
 
             entity.HasOne(d => d.CreatedByNavigation).WithMany(p => p.TaskHistories)
                 .HasForeignKey(d => d.CreatedBy)
