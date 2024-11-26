79dbb7384120703e9b27acf4ae151badc387b42f
Dak Lod <trandacloc123@gmail.com>
2024-11-26 16:00:30 +0700

    fix file download

--- server/Controllers/FileController.cs
+++ server/Controllers/FileController.cs
@@ -22,10 +22,32 @@ public class FileController : Controller
     public async Task<IActionResult> Index(string file_id)
     {
         var entity = _unitOfWork.FileRepository.GetById(file_id);
-        var file = entity.Path;
+        var file_path = Path.Combine(entity.Path, _uploadDirectory);
+        if (!Directory.Exists(file_path))
+            return new ErrorResponse("File does not exist");
         var stream = new FileStream(_uploadDirectory + "/" + entity.Path, FileMode.Open, FileAccess.Read, FileShare.Read);
-        if (stream == null)
-            return new ErrorResponse("File not found");
-        return File(stream, "application/octet-stream", entity.Path);
+        var contentType = GetContentType(stream.Name);
+
+        // Return the file as a FileResult
+        return File(stream, contentType, entity.Path);
+    }
+
+    [ApiExplorerSettings(IgnoreApi = true)]
+    private string GetContentType(string path)
+    {
+        var types = new Dictionary<string, string>
+        {
+            { ".txt", "text/plain" },
+            { ".pdf", "application/pdf" },
+            { ".jpg", "image/jpeg" },
+            { ".png", "image/png" },
+            { ".doc", "application/msword" },
+            { ".docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document" },
+            { ".xls", "application/vnd.ms-excel" },
+            { ".xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" }
+        };
+
+        var ext = Path.GetExtension(path).ToLowerInvariant();
+        return types.ContainsKey(ext) ? types[ext] : "application/octet-stream";
     }
 }
