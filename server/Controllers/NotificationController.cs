ebc45f165398b6dc9987881dbd615d100046ae1d
lehoang <tranbaolong00zz@gmail.com>
2024-12-08 16:26:07 +0700

    updated notification

--- server/Controllers/NotificationController.cs
+++ server/Controllers/NotificationController.cs
@@ -16,42 +16,27 @@ namespace server.Controllers;
 [Route("[controller]")]
 public class NotificationController : Controller
 {
-    private readonly INotificationService _notificationService;
-    public NotificationController(INotificationService notificationService)
+    private readonly IRepository<Notification> _repository;
+    public NotificationController(IUnitNotification _unitnotification)
     {
-        _notificationService = notificationService;
+        _repository = _unitnotification.Notification;
     }
-    
 
-    //lấy mọi notifications
-    // [HttpGet]
-    // public ActionResult<IEnumerable<Notification>> Get()
-    // {
-    //     return _notificationService.GetAllNotifications().ToList();
-        
-    // }
 
     [HttpGet]
-    public ActionResult<IEnumerable<Notification>> Get([FromQuery(Name = "filter")] string? filterString, string? includes = "")
+    public ActionResult<IEnumerable<Notification>> Get([FromQuery(Name = "filter")] string? filterString, int? page,
+        int? pageItem, string? includes = "")
     {
         var filter = new ClientFilter();
         if (!string.IsNullOrEmpty(filterString)) filter = JsonConvert.DeserializeObject<ClientFilter>(filterString);
         return new SuccessResponse<IEnumerable<Notification>>(
-            _notificationService.GetNotificationByFilter(CompositeFilter<Notification>.ApplyFilter(filter), includes));
+            _repository.Get(CompositeFilter<Notification>.ApplyFilter(filter), includeProperties: includes));
     }
 
     [HttpGet("{id}")]
-    public ActionResult<IEnumerable<Notification>> Get(string id)
+    public ActionResult<IEnumerable<Notification>> Get(long id, string? includes ="")
     {
-        try
-        {
-            return new SuccessResponse<Notification>(_notificationService.GetNotificationById(new Guid(id)));
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.ToString());
-            return new ErrorResponse("notification is not get");
-        }
+        return new SuccessResponse<Notification>(_repository.GetById(id.ToString(), includes));
     }
 
 
@@ -59,15 +44,10 @@ public class NotificationController : Controller
     [HttpPost]
     public ActionResult<Notification> CreateNotification(Notification notification)
     {
-        try
-        {
-            return new SuccessResponse<Notification>(_notificationService.CreateNotification(notification));
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.ToString());
-            return new ErrorResponse("Notification failed to create");
-        }
+        notification.UserId = new Guid(AuthController.GetUserId(HttpContext));
+        var entity = _repository.Add(notification);
+        _repository.Save();
+        return new SuccessResponse<Notification>(notification);
     }
 
 
@@ -76,42 +56,23 @@ public class NotificationController : Controller
 [HttpPut]
 public ActionResult<Notification> UpdateNotification(Notification notification)
 {
-    try
-        {
-            return new SuccessResponse<Notification>(_notificationService.UpdateNotification(notification));
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.ToString());
-            return new ErrorResponse("Notification did not update");
-        }
+    var entity = _repository.Update(notification);
+    _repository.Save();
+    return new SuccessResponse<Notification>(entity);
 }
 
 [HttpPatch("{id}")]
 public ActionResult<Notification> PatchNotification(long id, [FromBody]JsonPatchDocument<Notification> notification)
 {
-    try
-        {
-            return new SuccessResponse<Notification>(_notificationService.PatchNotification(id,notification));
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.ToString());
-            return new ErrorResponse("Notification did not patch");
-        }
+    return new SuccessResponse<Notification>(_repository.UpdatePatch(id, notification));
 }
 
 [HttpDelete]
     public ActionResult DeleteNotification(long id)
     {
-        try
-        {
-            return new SuccessResponse<Notification>(_notificationService.DeleteNotification(id));
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.ToString());
-            return new ErrorResponse("Notification did not delete");
-        }
+        var entity = _repository.GetById(id.ToString());
+        _repository.Remove(entity);
+        _repository.Save();
+        return new SuccessResponse<Notification>(entity);
     }
 }
